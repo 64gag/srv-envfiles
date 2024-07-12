dae385740ffc6ff27e21b522a1aa24881465d3c6
Gael Aguiar <gael.aguiar@kymasoft.com>
2024-07-12 19:06:53 +0200

    wip backup

---
+++ home/.local/bin/srv-lxc-2003-ubuntu-docker.doc.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+source "$(dirname "$0")/srv-lib.doc.sh"
+
+venv_id=2003
+venv_user_name=lxc-2003-ubuntu-docker
+
+venv_template=trinity-hdd-pve-isos-encrypted:vztmpl/ubuntu-23.10-standard_23.10-1_amd64.tar.zst
+venv_storage=trinity-hdd-pve-vms-encrypted
+venv_rootfs=trinity-hdd-pve-vms-encrypted:16
+venv_cores=4
+venv_memory_MB=$((16 * 1024))
+venv_net_if=name="eth0,bridge=vmbr0,hwaddr=$(srv_lib_generate_mac_address ${SRV_NET_MAC_ADDR_PREFIX} $venv_id),ip=dhcp"
+venv_pubkeys=/root/.ssh/authorized_keys
+venv_start_on_boot=1
+venv_lxc_unprivileged=1
+venv_features=nesting=1
+
+venv_mps_host_base_dir="${SRV_ZFS_POOLS_DIR}/${SRV_HDD_POOL_BASENAME}/${SRV_LXC_MPS_DATASET_BASENAME}/${venv_id}"
+venv_mp0_guest_dir="/var/lib/docker"
+venv_mp0_host_dir="${venv_mps_host_base_dir}${venv_mp0_guest_dir}"
+#TODO GAG what is missing (a mountpoint and some config?) to backup the "database"?
+
+script_basename="$(basename "$0")"
+
+case $1 in
+    "host-create")
+        srv_lib_add_group_and_user "${venv_id}" "${venv_user_name}"
+
+        srv_lib_venv_mp_create "${venv_user_name}" "${venv_mp0_host_dir}"
+
+        pct create ${venv_id} "${venv_template}" \
+            --hostname ${venv_user_name} \
+            --storage ${venv_storage} \
+            --rootfs ${venv_rootfs} \
+            --cores ${venv_cores} \
+            --memory ${venv_memory_MB} \
+            --net0 "${venv_net_if}" \
+            --ssh-public-keys "${venv_pubkeys}" \
+            --onboot ${venv_start_on_boot} \
+            --mp0 "${venv_mp0_host_dir},mp=${venv_mp0_guest_dir}" \
+            --unprivileged ${venv_lxc_unprivileged} \
+            --features "${venv_features}"
+
+        if [ $? -eq 0 ]; then
+            srv_lib_configure_lxc_idmaps $venv_id 100000 65536 0=$venv_id
+            srv_lib_mount_rootfs_and_change_ownership_to_new_lxc_idmaps "${venv_id}" "${venv_user_name}"
+
+            srv_lib_venv_mp_create "${venv_user_name}" "${venv_mp0_host_dir}"
+
+            srv_lib_start_and_create_guest ${venv_id} "$0"
+        fi
+        ;;
+    "host-purge")
+        pct stop ${venv_id}
+        pct destroy ${venv_id} --purge
+        srv_lib_remove_line_from_file "root:${venv_id}:1" "/etc/subuid"
+        srv_lib_remove_line_from_file "root:${venv_id}:1" "/etc/subgid"
+        srv_lib_remove_group_and_user "${venv_user_name}"
+        #rm -rf "${venv_mps_host_base_dir}" # NOTE commented for nextcloud because it is quite dangerous...
+        ;;
+    "guest-create")
+        apt update && apt upgrade -y
+        apt install -y avahi-daemon
+        srv_lib_install_docker_on_ubuntu
+        ;;
+    start|stop|destroy|mount|unmount|exec)
+        action=$1
+        shift
+        pct ${action} ${venv_id} "$@"
+        ;;
+    *)
+        echo "${script_basename}: Unsupported or unknown action $1"
+        ;;
+esac
---
+++ home/.local/bin/srv-lxc-2100-ubuntu-ollama.doc.sh
@@ -0,0 +1,10 @@
+root@ct-ubuntu23-ollama:~# history
+    1  ip addr
+    2  reboot
+    3  apt update && apt upgrade -y
+    4  apt install curl
+    5  curl https://ollama.ai/install.sh | sh
+    6  ollama run dolphin-mixtral
+    7  lscpu 
+    8  ollama run dolphin-mixtral
+    9  history
